727bbb7ef6d4660d9daf2e474c5ceddf1fa95e50
bechirzarroukietg <bechir.zarrouki@everythingtogain.com>
2025-12-11 13:41:46 +0100

    Add Spring Boot project nefzi-raed

---
+++ src/main/java/com/nefziraed/NefziRaedApplication.java
@@ -0,0 +1,13 @@
+package com.nefziraed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NefziRaedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NefziRaedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nefziraed/NefziRaedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nefziraed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NefziRaedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
